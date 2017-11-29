956ee3b9e9316a61d80dde2c8ad257d64401c0d4
Grant Moe <grant.moe@ndsu.edu>
2017-11-28 19:54:38 -0600

    adding OpenWeatherApi proj

---
+++ OpenWeatherAPI/Coord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherAPI
+{
+    public class Coord
+    {
+        private const string Lon = "lon";
+        private const string Lat = "lat";
+
+        public double Longitude { get; private set; }
+        public double Latitude { get; private set; }
+
+        public Coord(JToken coordData)
+        {
+            Longitude = double.Parse(coordData.SelectToken(Lon).ToString());
+            Latitude = double.Parse(coordData.SelectToken(Lat).ToString());
+        }
+    }
+}
